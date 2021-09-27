f0573d96be378570e1de92a9c0f79e6b3540fb19
Noodles Florence <alessandromolinari2000@hotmail.com>
2021-09-27 19:40:59 +0200

    small changes

--- main.py
+++ main.py
@@ -28,13 +28,11 @@ for pos in positions:
 while STILL_PLAYING:
     screen.update()
     time.sleep(0.1)
-    for seg in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg - 1].xcor()
-        new_y = segments[seg - 1].ycor()
-        segments[seg].goto(new_x, new_y)
-    segments[0].forward(10)
-
-
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 
