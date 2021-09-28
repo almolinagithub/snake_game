24834d2ce4b076e6c6f7eca6ec5065e4954adc83
Noodles Florence <alessandromolinari2000@hotmail.com>
2021-09-28 20:12:44 +0200

    1st refactoring

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 # TODO 0: CREATE THE SNAKE BODY
 
 import time
+from snake import Snake
 from turtle import Turtle, Screen
 
-STILL_PLAYING = True
-x = 0
-y = 0
-positions = [(x, y), (x - 20, y), (x - 40, y)]
-segments = []
 
+STILL_PLAYING = True
+
+snake = Snake()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title(titlestring="My snake game")
 screen.bgcolor("black")
 screen.tracer(0)
 
-for pos in positions:
-    new_segment = Turtle()
-    new_segment.shape("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(pos)
-    segments.append(new_segment)
 
 #TODO 1: MOVE THE SNAKE ALWAYS FORWARD
 
 while STILL_PLAYING:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.create_snake()
+
 
 
 
